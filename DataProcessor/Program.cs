c6480675c5328fe46c96688e8e3a94fc44cfa6a8
Ivan MSI <arigatory@gmail.com>
2021-03-14 11:18:10 +0300

    FileProcessor add

--- DataProcessor/Program.cs
+++ DataProcessor/Program.cs
@@ -10,6 +10,42 @@ namespace DataProcessor
     {
         static void Main(string[] args)
         {
+
+            Console.WriteLine("Parsing command line options");
+
+            var command = args[0];
+
+            if (command == "--file")
+            {
+                var filePath = args[1];
+                Console.WriteLine($"Single file {filePath} selected");
+                ProcessSingleFile(filePath);
+            }
+            else if (command == "--dir")
+            {
+                var directoryPath = args[1];
+                var fileType = args[2];
+                Console.WriteLine($"Directory {directoryPath} selected for {fileType} files");
+                ProcessDirectory(directoryPath, fileType);
+            }
+            else
+            {
+                Console.WriteLine("Invalid command line options");
+            }
+
+            Console.WriteLine("Press enter to quit.");
+            Console.ReadLine();
+        }
+
+        private static void ProcessDirectory(string directoryPath, string fileType)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void ProcessSingleFile(string filePath)
+        {
+            var fileProcessor = new FileProcessor(filePath);
+            fileProcessor.Process();
         }
     }
 }
